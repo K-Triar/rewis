422e6c88a60d6f9089401fb41d25af93596f535a
gorodrich <gorodrich@gmail.com>
2026-09-17 20:06:04 +0900

    画像パス修正

--- editor-graph/main.js
+++ editor-graph/main.js
@@ -67,7 +67,7 @@ function renderHeader() {
   const session = api.getSavedSession();
 
   const left = h('div', { class: 'g-header__left' },
-    h('img', { class: 'g-header__logo', src: 'assets/icons/rewis_logo_w.svg', alt: '' }),
+    h('img', { class: 'g-header__logo', src: '../assets/icons/rewis_logo_w.svg', alt: '' }),
     h('div', { class: 'g-header__title' },
       '| 路線データ編集'
     )
